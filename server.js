228c93b98bc11e99bea198719e8747acd8165a72
mpj1693 <mpj1693@gmail.com>
2019-05-07 23:55:35 -0400

    server.js and all packages added

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+require('./routes/htmlRoutes')(app);
+require('./routes/apiRoutes')(app);
+
+app.get('*', function(req, res) {
+  res.json({
+    status: 404,
+    message: "You've come to the wrong place!"
+  });
+});
+
+app.listen(PORT, () => console.log(`You are online on port #${PORT}`));
