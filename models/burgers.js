894f3187793954c10cd7092eb8e57898dceb2ef1
mpj1693 <mpj1693@gmail.com>
2019-05-08 00:07:33 -0400

    models

---
+++ models/burgers.js
@@ -0,0 +1,81 @@
+const connection = require('./connection');
+
+const findAll = () => {
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT * FROM burgers', function(err, dbBurgerData) {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(dbBurgerData);
+    });
+  });
+};
+
+const findById = burgerId => {
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT * FROM burgers WHERE id = ?', [burgerId], function(err, dbBurgerData) {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(dbBurgerData);
+    });
+  });
+};
+
+const create = burgerDataObj => {
+  return new Promise((resolve, reject) => {
+    connection.query('INSERT INTO burgers SET ?', [burgerDataObj], function(err, dbBurgerData) {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(dbBurgerData);
+    });
+  });
+};
+
+const update = (eatenValue, burgerId) => {
+  return new Promise((resolve, reject) => {
+
+    eatenValue = (eatenValue === "true") 
+      ? true : false;
+
+    connection.query("UPDATE burgers SET eaten = ? WHERE id = ?", [eatenValue, burgerId], function(err, dbBurgerData) {
+
+      if (err) {
+        return reject(err);
+      }
+      else if (dbBurgerData.changedRows === 0) {
+        return reject({message: "You probably have the wrong ID"});
+      }
+      else {
+        return resolve(dbBurgerData);
+      }
+    })
+  })
+}
+
+const remove = (burgerId) => {
+  return new Promise((resolve, reject) => {
+
+    connection.query("DELETE FROM burgers WHERE id = ?", [burgerId], function (err, dbBurgerData) {
+
+      if (err) {
+        return reject(err);
+      }
+      else if (dbBurgerData.affectedRows === 0) {
+        return reject({ message: "You probably have the wrong ID" });
+      }
+      else {
+        return resolve(dbBurgerData);
+      }
+    })
+  })
+}
+
+module.exports = {
+  findAll,
+  findById,
+  create,
+  update,
+  remove
+};
